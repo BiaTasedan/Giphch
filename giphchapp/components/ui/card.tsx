dfc70de5af710b9a706814b451428e4144f0dd47
BiaMotoc <bianca.tasedan04@gmail.com>
2025-04-10 12:27:49 +0300

    add features(api call-api key, card component) and add styling w/ Tailwind

---
+++ giphchapp/components/ui/card.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function cn(...classes: (string | undefined | null | false)[]): string {
+  return classes.filter(Boolean).join(" ");
+}
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Card({ className, ...props }: CardProps) {
+  return (
+    <div
+      className={cn("rounded-2xl border bg-white text-black shadow-sm", className)}
+      {...props}
+    />
+  );
+}
+
+interface CardContentProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function CardContent({ className, ...props }: CardContentProps) {
+  return <div className={cn("p-4", className)} {...props} />;
+}
